fd1953fa893d9000d04232e239ab889057d1705a
松本 太一 <taichino@Flute.local>
2011-01-17 08:26:17 +0900

    add operator++(int)

--- microtree.h
+++ microtree.h
@@ -1,8 +1,31 @@
+/* 
+ * Copyright 2011 Matsumoto Taichi
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 #pragma once
 
 #include <iostream>
 #include <map>
-#include "picojson.h"
+#include "lib/picojson.h"
 
 namespace microtree {
 
@@ -59,6 +82,7 @@ class tree {
 	bool operator==(const dfs_iterator& itr) const;
 	bool operator!=(const dfs_iterator& itr) const;
 	dfs_iterator operator++();
+	dfs_iterator operator++(int);
     };
   public:
     typedef dfs_iterator iterator;
@@ -315,5 +339,9 @@ tree::dfs_iterator tree::dfs_iterator::operator++() {
     }
     return *this;
 }
-
+tree::dfs_iterator tree::dfs_iterator::operator++(int) {
+    ++(*this);
+    return *this;
+}
+ 
 } // end of namespace microtree
